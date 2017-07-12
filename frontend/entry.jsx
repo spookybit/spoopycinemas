cf895950cdac0f362754c4c438cd612a56bfdd53
Vincent Chan <vincent.chan@biola.edu>
2017-07-12 16:45:06 -0700

    create store in frontend

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -3,7 +3,19 @@ import ReactDOM from 'react-dom';
 
 import Root from './components/root';
 
+import configureStore from './store/store';
+
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  ReactDOM.render(<Root />, root);
+  let store;
+
+  if (window.currentUser) {
+    const preloadedState = {session: {currentUser: window.currentUser}};
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+  
+  ReactDOM.render(<Root store={store}/>, root);
 });
---
+++ frontend/store/store.js
@@ -0,0 +1,11 @@
+import { compose, createStore, applyMiddleware } from 'redux';
+import rootReducer from '../reducers/root_reducer';
+import thunk from 'redux-thunk';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const configureStore = (preloadedState = {}) => (
+  createStore(rootReducer, preloadedState, composeEnhancers(applyMiddleware(thunk)))
+);
+
+export default configureStore;
