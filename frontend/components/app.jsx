930828003005c6c60867f0281e7c96a84f609dad
Vincent Chan <vincent.chan@biola.edu>
2017-07-12 16:10:33 -0700

    loaded frontend react root file

---
+++ frontend/components/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Route, Redirect, Switch, Link, HashRouter} from 'react-router-dom';
+
+const App = () => (
+  <div>
+    <h1>lol</h1>
+  </div>
+);
+
+export default App;
