3c7cc4cd35fe69ea5a0e1a03629637da825fa4ca
Vincent Chan <vincent.chan@biola.edu>
2017-07-18 10:26:31 -0700

    login and logout action

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -14,10 +14,23 @@ export const receiveErrors = errors => ({
   errors
 });
 
-export const signup = user => (
+export const signup = user => dispatch => (
   APIUtil.signup(user)
     .then(data => dispatch(receiveCurrentUser(data))
   ), err => (
     dispatch(receiveErrors(err.responseJSON))
   ))
+);
+
+export const login = user => dispatch => (
+  APIUtil.login(user)
+    .then(data => (dispatch(receiveCurrentUser(data))
+  ), err => (
+    dispatch(receiveErrors(err.responseJSON))
+  ))
+);
+
+export const logout = () => dispatch (
+  APIUtil.logout()
+    .then(() => dispatch(receiveCurrentUser(null)))
 );
